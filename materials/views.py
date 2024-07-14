3ce9f5db72f835d142eafceb8eaa69afdc40cfc6
DarkRazec <darkrazec@yahoo.com>
2024-07-14 15:39:26 +0300

    fixed perms

--- materials/views.py
+++ materials/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 
 from materials.models import Course, Lesson
-from users.permissions import IsOwnerOrStaff
+from users.permissions import IsStaff, IsAuthor
 from materials.serializer import CourseSerializer, LessonSerializer
 
 
@@ -16,12 +16,10 @@ class CourseViewSet(viewsets.ModelViewSet):
         self.object.save()
 
     def get_permissions(self):
-        if self.action == 'create':
-            permission_classes = [IsOwnerOrStaff]
-        elif self.action == 'destroy':
-            permission_classes = [IsOwnerOrStaff]
+        if self.action in ('create', 'destroy'):
+            permission_classes = [~IsStaff, IsAuthor]
         elif self.action in ('update', 'retrieve'):
-            permission_classes = [IsOwnerOrStaff]
+            permission_classes = [IsStaff | IsAuthor]
         else:
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
@@ -29,7 +27,7 @@ class CourseViewSet(viewsets.ModelViewSet):
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsOwnerOrStaff]
+    permission_classes = [~IsStaff, IsAuthenticated]
 
     def perform_create(self, serializer):
         self.object = serializer.save()
@@ -46,15 +44,15 @@ class LessonListAPIView(generics.ListAPIView):
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsOwnerOrStaff]
+    permission_classes = [IsStaff | IsAuthor]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsOwnerOrStaff]
+    permission_classes = [IsStaff | IsAuthor]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
-    permission_classes = [IsOwnerOrStaff]
+    permission_classes = [~IsStaff, IsAuthor]
--- users/permissions.py
+++ users/permissions.py
@@ -1,6 +1,11 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsOwnerOrStaff(BasePermission):
+class IsStaff(BasePermission):
+    def has_permission(self, request, view) -> bool:
+        return request.user.groups.filter(name='moderator').exists() or request.user.is_superuser
+
+
+class IsAuthor(BasePermission):
     def has_object_permission(self, request, view, obj) -> bool:
-        return request.user.groups.filter(name='moderator').exists() or request.user == obj.author
+        return request.user == obj.author or request.user.is_superuser
